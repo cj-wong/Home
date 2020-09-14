a6511f1450feab9ea38966460a861da34568fd6f
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2020-09-14 15:39:08 -0700

    Check whether existing files are symlink and move them if they don't point to intended files

--- install.sh
+++ install.sh
@@ -23,29 +23,43 @@ FILE_BLACKLIST=(
 echo "?"
 
 for file in * .[^.]*; do
-    echo "Checking $file..."
-    home_file="$HOME/$file"
+    echo "Checking ${file}..."
+    home_file="${HOME}/${file}"
+    repo_file="${REPO_DIR}/${file}"
     if [[ -d "$file" && "${DIR_BLACKLIST[@]}" =~ "$file" ]]; then
-        echo "$file is in the blacklist for directories. Skipping."
+        echo "${file} is in the blacklist for directories. Skipping."
         continue
     elif [[ -f "$file" && "${FILE_BLACKLIST[@]}" =~ "$file" ]]; then
-        echo "$file is in the blacklist for files. Skipping."
+        echo "${file} is in the blacklist for files. Skipping."
         continue
     fi
 
-    if [ -d "$home_file" ]; then
+    if [ -h "$home_file" ]; then
+        resolved=$(readlink "$home_file")
+        if [ ! -z "$resolved" ]; then
+            if [ "$resolved" = "$repo_file" ]; then
+                echo "${home_file} already points to the right file. Skipping."
+                continue
+            else
+                echo "${home_file} points to a different file."
+                echo "It will be moved."
+                copy_tmp "$home_file"
+                rm -f "$home_file"
+            fi
+        fi
+    elif [ -d "$home_file" ]; then
         tmp=$(mktemp -p ~ -d)
-        echo "$home_file exists and will be moved to $tmp."
+        echo "${home_file} exists and will be moved to ${tmp}."
         mv "$home_file" "$tmp"
     elif [ -e "$home_file" ]; then
-        echo "$home_file exists as a file and will be moved."
+        echo "${home_file} exists as a file and will be moved."
         copy_tmp "$home_file"
         rm -f "$home_file"
     fi
-    ln -s "$REPO_DIR/$file" ~
+    ln -s "$repo_file" ~
 done
 
-echo "Changing repository directory permissions to 700"
+echo "Changing repository directory permissions to 700."
 chmod 700 "$REPO_DIR"
 
 cd "$LAST_DIR"
