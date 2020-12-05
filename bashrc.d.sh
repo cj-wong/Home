f3bc87ab8de1e6079fd9c0c67d6815e6f5b0804a
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2020-12-05 01:44:56 -0800

    Resolve #2; fix .bashrc.d detection in .bashrc

--- bashrc.d.sh
+++ bashrc.d.sh
@@ -2,6 +2,7 @@
 #
 # Injects .bashrc.d functionality into .bashrc
 
+
 INJECT_BASHRCD=$(cat <<END
 .bashrc.d functionality was not detected in your .bashrc file.
 If injected, your old .bashrc will be saved to a temporary file.
@@ -19,13 +20,13 @@ END
 )
 
 # Compare whether the block of text in .bashrc matches $TEXT here.
-IFS=$'\n' ARR=("$TEXT")
-TEXT_LEN=$(echo "$TEXT" | wc -l)
-TEXT_BASHRC=$(grep -A "$TEXT_LEN" -F "${ARR[0]}" ~/.bashrc)
+# IFS=$'\n' ARR=("$TEXT")
+# TEXT_LEN=$(echo "$TEXT" | wc -l)
+# TEXT_BASHRC=$(grep -A "$TEXT_LEN" -F "${ARR[0]}" ~/.bashrc)
 
 # If the injection block isn't found in ~/.bashrc, ask the user whether to
 # inject the block to allow .bashrc.d functionality.
-if [[ "$TEXT" != "$TEXT_BASHRC" ]]; then
+if [[ $(< "${HOME}/.bashrc") != *"$TEXT"* ]]; then
     read -r -p "$INJECT_BASHRCD" prompt
     if [[ $prompt =~ ^[yY] ]]; then
         if ! type -t copy_tmp > /dev/null 2>&1; then
